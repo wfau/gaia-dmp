90b8ddc27561d256d08a6498c448140b6b9a7e20
Dave Morris <zrq-github@metagrid.co.uk>
2020-08-10 05:55:24 +0100

    Works for Java client

--- experiments/zrq/java/spark-tools/src/test/java/uk/ac/roe/wfau/aglais/aws/AWSTestCase.java
+++ experiments/zrq/java/spark-tools/src/test/java/uk/ac/roe/wfau/aglais/aws/AWSTestCase.java
@@ -1,4 +1,4 @@
-/*
+4/*
  *  Copyright (C) 2020 Royal Observatory, University of Edinburgh, UK
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -21,29 +21,18 @@ package uk.ac.roe.wfau.aglais.aws;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Iterator;
-import java.util.List;
 
 import org.apache.hadoop.conf.Configuration ;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
-import org.apache.hadoop.fs.s3a.DefaultS3ClientFactory;
 import org.apache.hadoop.fs.s3a.S3AFileSystem;
-import org.apache.hadoop.fs.s3a.S3ClientFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider ;
-import com.amazonaws.internal.StaticCredentialsProvider;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 import junit.framework.TestCase;
 import lombok.extern.slf4j.Slf4j;
@@ -63,43 +52,47 @@ extends TestCase
     {
     /**
      * Public constructor.
-     * 
+     *
      */
     public AWSTestCase()
         {
         super();
         }
-        
+
     /**
      * Test things.
-     * @throws URISyntaxException 
-     * @throws IOException 
-     * 
+     * @throws URISyntaxException
+     * @throws IOException
+     *
      */
     @Test
     public void test002() throws URISyntaxException, IOException
         {
+        log.info("Starting test run");
         /*
-         * 
+         *
         AWSCredentialsProvider creds = new StaticCredentialsProvider(
             new BasicAWSCredentials(
                 "",
                 ""
                 )
             );
-         * 
+         *
          */
-        
+
         Configuration configuration = new Configuration();
 
-        configuration.set("fs.s3a.endpoint", "https://cumulus.openstack.hpc.cam.ac.uk:6780/swift/v1/AUTH_21b4ae3a2ea44bc5a9c14005ed2963af/");
-        configuration.set("fs.s3a.access.key", "<empty>");
-        configuration.set("fs.s3a.secret.key", "<empty>");
+        //configuration.set("fs.s3a.endpoint", "https://cumulus.openstack.hpc.cam.ac.uk:6780/swift/v1/AUTH_21b4ae3a2ea44bc5a9c14005ed2963af/");
+        configuration.set("fs.s3a.endpoint", "https://cumulus.openstack.hpc.cam.ac.uk:6780");
+        //configuration.set("fs.s3a.access.key", "");
+        //configuration.set("fs.s3a.secret.key", "");
+        configuration.set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider");
         configuration.set("fs.s3a.path.style.access", "true");
         configuration.set("fs.s3a.bucket.probe", "0");
-        
+
         URI baseuri = new URI(
             "s3a://gaia-dr2-parquet"
+//            "s3a://albert"
             );
 
         S3AFileSystem fs = (S3AFileSystem) FileSystem.get(

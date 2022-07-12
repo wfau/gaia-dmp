5493c96bcce60c331a5e66efbaf1baa7d6972693
Zarquan <zarquan@metagrid.co.uk>
2022-07-12 04:03:58 +0100

    Starting create-ceph-share

---
+++ deployments/aglais/bin/create-ceph-share.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+# <meta:header>
+#   <meta:licence>
+#     Copyright (c) 2022, ROE (http://www.roe.ac.uk/)
+#
+#     This information is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This information is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   </meta:licence>
+# </meta:header>
+#
+#
+
+srcfile="$(basename ${0})"
+srcpath="$(dirname $(readlink -f ${0}))"
+
+# Include our JSON formatting tools.
+source "${srcpath}/json-tools.sh"
+
+username=${1}
+usertype=${2}
+
+# Check required params
+if [ -z "${username}" ]
+then
+    jsonerror "[username] required"
+    exit 1
+fi
+
+if [ -z "${usertype}" ]
+then
+    jsonerror "[usertype] required"
+    exit 1
+fi
+
+
+
+
+cat << EOF
+{
+"uuid":  "${cephuuid}",
+"path":  "${mountpath}",
+"owner": "${fileowner}",
+"group": "${filegroup}",
+$(jsondebug)
+}
+EOF
+

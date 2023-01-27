a909650e70b937e34f7935cfa6ef1fed5e198ded
stvoutsin <steliosvoutsinas@yahoo.com>
2023-01-27 18:20:45 +0200

    Add confirmation check before deleting live

--- deployments/hadoop-yarn/bin/deploy.sh
+++ deployments/hadoop-yarn/bin/deploy.sh
@@ -19,6 +19,21 @@
 # </meta:header>
 #
 
+# ----------------------------------------------------------------
+# Check if we are deleting live, confirm before continuing if yes
+
+
+live_hostname=$(ssh  -o "StrictHostKeyChecking no" fedora@live.gaia-dmp.uk 'hostname')
+
+if [[ "$live_hostname" == *"$cloudname"* ]]; then
+    read -p "You are deleting the current live system!! Do you want to proceed? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY != "y" ]];
+    then
+        exit
+    fi
+fi
+
 # -----------------------------------------------------
 # Delete everything.
 #[root@ansibler]

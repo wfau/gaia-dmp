b49896dded623b6b20d7c98c5739c58a8b4a908a
Stelios Voutsinas <steliosvoutsinas@yahoo.com>
2023-06-02 13:35:29 +0300

    Updating expected test duration, version of testing tool

--- deployments/hadoop-yarn/bin/deploy.sh
+++ deployments/hadoop-yarn/bin/deploy.sh
@@ -169,7 +169,7 @@ EOF
 # Install our integration tests.
 #[root@ansibler]
 
-    pip install git+https://github.com/wfau/aglais-testing@v0.2.6
+    pip install git+https://github.com/wfau/aglais-testing@v0.2.7
 
 
 # -----------------------------------------------------
